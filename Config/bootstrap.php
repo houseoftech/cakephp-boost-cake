a89c3a7f262586a39e9e5c934b9befe21e480866
Corie Slate <corie@houseoftech.com>
2013-11-27 22:04:14 -0500

    Added more default options

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -12,15 +12,45 @@ Configure::write('BoostCake.bootstrap_version', '3');
 Configure::write('BoostCake.inputDefaults', array(
 	'2' => array(
 		'default' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-error'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => false,
 			'wrapInput' => false
 		),
 		'form-inline' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-error'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => false,
 			'label' => false,
 			'wrapInput' => false
 		),
 		'form-horizontal' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-error'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => 'form-group',
 			'label' => array(
 				'class' => 'control-label'
@@ -30,17 +60,47 @@ Configure::write('BoostCake.inputDefaults', array(
 	),
 	'3' => array(
 		'default' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-danger'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => 'form-group',
 			'wrapInput' => false,
 			'class' => 'form-control'
 		),
 		'form-inline' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-danger'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => 'form-group',
 			'label' => false,
 			'wrapInput' => false,
 			'class' => 'form-control'
 		),
 		'form-horizontal' => array(
+			'error' => array(
+				'attributes' => array(
+					'wrap' => 'span',
+					'class' => 'help-block text-danger'
+				)
+			),
+			'checkboxDiv' => 'checkbox',
+			'beforeInput' => '',
+			'afterInput' => '',
+			'errorClass' => 'has-error error',
 			'div' => 'form-group',
 			'label' => array(
 				'class' => 'col col-md-3 control-label'
